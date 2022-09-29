e1335c59bc5180e70296b5dcf3a2e0878d6bcbc5
KatyKedi <katykat5bros@gmail.com>
2022-09-29 12:21:52 -0700

    async/await

---
+++ asyncAwait.js
@@ -0,0 +1,23 @@
+// Synchronous vs Asynchronous functions
+// Example: fetch request
+
+// Synchronous function
+function getApiSync() {
+    fetch('https://www.url.com/api')
+        .then(response => response.json())
+        .then(data => {
+              console.log(data)
+        })
+        console.log('Sync Test')
+}
+
+// Asynchronous function
+async function getApiAsync() {
+    const response = await fetch('https://www.url.com/api')
+    const data = await response.json()
+    console.log(data)
+    console.log('Async Test')
+}
+
+getApiSync()
+getApiAsync()
