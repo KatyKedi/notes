04b845858003095acefb6241af5e14c4ca6c179d
KatyKedi <katykat5bros@gmail.com>
2022-09-29 12:33:25 -0700

    conditional statements

---
+++ conditionalStatements.js
@@ -0,0 +1,23 @@
+// Conditional statements
+// If/else vs Switch vs Ternary Operators
+
+const condition = 'Raining'
+
+// If else blocks
+if (condition) {
+    console.log(condition)
+} else {
+    console.log('No condition')
+}
+
+// Switch statements
+switch (condition) {
+    case true:
+        console.log(condition)
+        break;
+    default:
+        console.log('No condition')
+}
+
+// Ternary Operators
+condition ? console.log(condition) : console.log('No condition')
